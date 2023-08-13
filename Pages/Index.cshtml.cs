54ec99d06b8329b777942eb19e108a10dad364b4
twdeveloper <twdeveloper@outlook.com>
2023-08-13 21:57:42 +0800

    v1

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+#region ForAuth
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+#endregion
+namespace az204_webappauth.Pages;
+
+public class IndexModel : PageModel
+{
+    public string UserName = "";
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        #region ForAuth
+        UserName = Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"].ToString();
+        //set user name in Authentication.Cookies
+        var claims = new List<Claim>
+                {
+                    //use email or LINE user ID as login name
+                    new Claim(ClaimTypes.Name,UserName),
+                    //other data
+                    new Claim("FullName",UserName),
+                    new Claim(ClaimTypes.Role, "nobody"),
+                };
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+        };
+
+        HttpContext.SignInAsync(
+           CookieAuthenticationDefaults.AuthenticationScheme,
+           new ClaimsPrincipal(claimsIdentity),
+           authProperties);
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+#region ForAuth
+using Microsoft.AspNetCore.Authentication.Cookies;
+#endregion
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+#region ForAuth
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+#endregion
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+#region ForAuth
+app.UseCookiePolicy();
+app.UseAuthentication();
+#endregion
+app.MapRazorPages();
+
+app.Run();
